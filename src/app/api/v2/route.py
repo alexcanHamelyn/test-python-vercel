e2b3aed2654f8030bbcdaaaf3ce7eac9dd332aed
Alex Cantón <107461147+alexcanHamelyn@users.noreply.github.com>
2024-10-17 09:12:13 +0200

    Add python code without vercel.json

--- src/app/api/v2/route.py
+++ src/app/api/v2/route.py
@@ -1,18 +1,15 @@
 import json
-from http.server import BaseHTTPRequestHandler
 
-class handler(BaseHTTPRequestHandler):
+def handler(event, context):
+    # Crear el objeto JSON
+    response = {
+        "message": "Hello from the Python API!"
+    }
 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-        
-        # Crear el objeto JSON
-        response = {
-            "message": "Hello from the Python API!"
-        }
-        
-        # Enviar la respuesta en formato JSON
-        self.wfile.write(json.dumps(response).encode('utf-8'))
-        return
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps(response)
+    }
